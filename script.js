34a1cef2a928c5cb0c685dec8a2c8156c7029003
Rishab-Rawat <rawatrishab66@gmail.com>
2024-08-22 20:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//complete this code
 class Person {
   constructor(name, age) {
     this._name = name;
@@ -9,28 +8,19 @@ class Person {
     return this._name;
   }
 
-  set age(newAge) {
-    this._age = newAge;
+  set age(age) {
+    this._age = age;
   }
 }
+
 class Student extends Person {
   study() {
     console.log(`${this.name} is studying`);
   }
 }
+
 class Teacher extends Person {
   teach() {
     console.log(`${this.name} is teaching`);
   }
 }
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
-
-person.age = 30;
-console.log(person._age); // Output: 30 (Note: Accessing _age directly, as no getter for age was provided)
-
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
